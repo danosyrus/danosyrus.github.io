de01495823129bf2da8ee9f1a9b5e4dab6961006
Dano Syrus <92238288+danosyrus@users.noreply.github.com>
2024-04-22 21:40:09 +0530

    Delete src/scripts/about.js

--- src/scripts/about.js
+++
@@ -1,7 +0,0 @@
-var modal = document.getElementById("modal");
-window.onclick = function(event) {
-  if (event.target == modal) {
-    window.alert(modal)
-    modal.style.display = "none";
-  };
-};
