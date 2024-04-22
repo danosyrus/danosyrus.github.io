3b7c1692e6ff400bcd3d68d9049df3b7e2dbd297
Dano Syrus <92238288+danosyrus@users.noreply.github.com>
2024-04-22 21:40:21 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,37 @@
 var texts = [
-    "Full Stack Developer",
-    "Web Designer",
-    "Intermediate Programmer",
-    "Tech Enthusiast"
-  ];
-  
-  var index = 0;
-  function changeText() {
-    var textElement = document.getElementById("bio");
-    textElement.classList.remove("show");
-  
-    setTimeout(function() {
-      textElement.textContent = texts[index];
-      index++;
-      if (index >= texts.length) {
-        index = 0;
-      }
-  
-      textElement.classList.add("show");
-    }, 500); 
-  }
-  
-  changeText();
-  setInterval(changeText, 3000);
-  
+  "Full Stack Developer",
+  "Web Designer",
+  "Programmer",
+  "Tech Enthusiast"
+];
+
+var index = 0;
+function changeText() {
+  var textElement = document.getElementById("bio");
+  textElement.classList.remove("show");
+
+  setTimeout(function () {
+    textElement.textContent = texts[index];
+    index++;
+    if (index >= texts.length) {
+      index = 0;
+    }
+
+    textElement.classList.add("show");
+  }, 500);
+}
+
+changeText();
+setInterval(changeText, 3000);
+
+fetch("src/config/config.json")
+  .then((res) => {
+    return res.json();
+  })
+  .then(data => {
+    const entries = Object.entries(data);
+    for (let [key] of entries) {
+      var res = document.getElementById(key);
+      res ? res.href = data[key] : null;
+    }
+  });
